0fbeb7d7a17013b1b74041ba38f4905ba062cdb1
Станислав Сиренко <stas@MacBook-Air-Stanislav.local>
2022-11-24 09:45:13 +0500

    sixth commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import './index.css'
 
 import TodoApp from './components/TodoApp'
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'))
+createRoot(document.getElementById('root')).render(<TodoApp />)
